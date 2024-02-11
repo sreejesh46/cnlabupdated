e89444647a92c1abc966ff8f665e49be079ce863
Sreejesh Mohan <86306486+sreejesh46@users.noreply.github.com>
2024-02-11 22:35:53 +0530

    client.c
    TCP CLIENT

--- client.c
+++ client.c
@@ -1,60 +1,54 @@
-/*
-First run server.c by gcc server.c -o server
-then run server by ./server
-After that start client by  gcc client.c -o client
-then run client by ./client
-*/
+Here's the beautified version of the code with proper indentation and spacing:
 
-#include<stdio.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<strings.h>
-#include<unistd.h>
-int main()
-{
-    int clientsocket,port; //clientsocket is the socket descriptor , port is the port number
-    struct sockaddr_in serveraddr; //creating a structure of type sockaddr_in for server
-    socklen_t len; //creating a variable to store the length of the server address
-    char message[50];
+```c
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
+int main() {
+    int socket_desc;
+    struct sockaddr_in server_addr, client_addr;
+    char client_message[2000], server_message[2000];
 
-    clientsocket=socket(AF_INET,SOCK_STREAM,0);//creating a socket
-    bzero((char*)&serveraddr,sizeof(serveraddr));//initializing the server address to zero
-    len=sizeof(serveraddr); //storing the length of the server address in len
-    serveraddr.sin_family=AF_INET; //setting the family of the server address to AF_INET
+    memset(client_message, '\0', sizeof(client_message));
+    memset(server_message, '\0', sizeof(server_message));
 
-    printf("Enter the port number ");
-    scanf("%d",&port);
-    serveraddr.sin_port=htons(port);//setting the port number of the server address to port
-    printf("\nTrying to connect to the server.\n");
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
+    if (socket_desc < 0) {
+        printf("\nError while creating the socket..");
+        return -1;
+    }
+    printf("\nSocket created successfully.");
 
-    connect(clientsocket,(struct sockaddr*)&serveraddr,sizeof(serveraddr));//connecting to the server
-    printf("\nConnected to the server.\n");
-    printf("\nSending message for server connection");
-    
-    send(clientsocket,"HI,IAM CLIENT...",sizeof("HI,IAM CLIENT..."),0);//sending the message to the server
-    printf("\nReceiving message from server.\n");
-    recv(clientsocket,message,sizeof(message),0);//receiving the message from the server
-    printf("\nMessage received.\t%s\n",message);
-    close(clientsocket);//closing the socket
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(2000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(socket_desc, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("\nCannot connect...");
+        return -1;
+    }
+
+    printf("\nConnected successfully.");
+
+    printf("\nEnter the message: ");
+    gets(client_message);
+
+    if (send(socket_desc, client_message, strlen(client_message), 0) < 0) {
+        printf("\nCannot send the message");
+        return -1;
+    }
+
+    if (recv(socket_desc, server_message, sizeof(server_message), 0) < 0) {
+        printf("\nCould not receive the message...");
+        return -1;
+    }
+
+    printf("\nServer res: %s\n", server_message);
+
+    close(socket_desc);
+    return 0;
 }
-
-/*
-
-s6d2@user-HP-280-G3-MT:~/Networking-Lab-S6/Socket-Programming/TCP$ gcc client.c -o client
-s6d2@user-HP-280-G3-MT:~/Networking-Lab-S6/Socket-Programming/TCP$ ./client
-Enter the port number 6000
-
-Trying to connect to the server.
-
-Connected to the server.
-
-Sending message for server connection
-Receiving message from server.
-
-Message received.       YOUR MESSAGE RECEIVED.
-s6d2@user-HP-280-G3-MT:~/Networking-Lab-S6/Socket-Programming/TCP$ 
-
-*/
+```
