5a05d4dca2bb655b3da50d2cf1534793a6ee0df6
Sreejesh Mohan <86306486+sreejesh46@users.noreply.github.com>
2024-02-11 15:23:47 +0530

    Add files via upload

---
+++ palinc.c
@@ -0,0 +1,50 @@
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+
+int main() {
+    struct sockaddr_in client;
+    int s, flag;
+    char buffer[20];
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    client.sin_family = AF_INET;
+    client.sin_port = htons(2000); 
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    connect(s, (struct sockaddr*)&client, sizeof(client));
+
+    printf("\nEnter a string to check palindrome: "); 
+    scanf("%s", buffer); 
+    printf("\nClient: %s", buffer); 
+    send(s, buffer, sizeof(buffer), 0); 
+    recv(s, &flag, sizeof(int), 0); 
+
+    if (flag == 1) 
+        printf("\nServer: The string is a Palindrome.\n"); 
+    else 
+        printf("\nServer: The string is not a palindrome.\n"); 
+
+    close(s); 
+    return 0;
+}
+
+/* ALOGRITHM
+1. Create a TCP socket (s).
+2. Configure the server address:
+    - Set the address family to AF_INET.
+    - Set the port to 2000 (converted to network byte order).
+    - Set the IP address to localhost (127.0.0.1).
+3. Connect to the server using the configured server address.
+4. Prompt the user to enter a string to check for palindrome.
+5. Read the string input from the user.
+6. Send the string to the server for palindrome checking.
+7. Receive the result (flag) from the server.
+8. Check the flag received from the server:
+    - If the flag is 1, print "The string is a Palindrome."
+    - Otherwise, print "The string is not a palindrome."
+9. Close the socket.
+10. Exit the program.
+*/
